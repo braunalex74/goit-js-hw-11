14e2a592fd76ba306354790f7edcec2cb6e1bb0d
braunalex74 <braunalex74@googlemail.com>
2023-04-09 22:10:09 +0300

    DZ_11

--- src/index.js
+++ src/index.js
@@ -1,91 +1,23 @@
 import Notiflix from 'notiflix';
 import axios from 'axios';
 
-const API_KEY = '';
+import Notiflix from 'notiflix';
+import axios from 'axios';
+
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 
 let searchQuery = '';
 let page = 1;
-// let isLastPage = false;
-
-// form.addEventListener('submit', handleSubmit);
-// loadMoreBtn.addEventListener('click', handleMore);
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   searchQuery = event.target.elements.searchQuery.value.trim();
-//   if (!searchQuery) {
-//     return;
-//   }
-//   clearGallery();
-//   page = 1;
-//   isLastPage = false;
-//   fetchImages();
-// }
-
-// function handleMore() {
-//   page += 1;
-//   fetchImages();
-// }
-
-// function fetchImages() {
-//   const url = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&emage_type=photo&orientation=horizontal&safesearch=true&page=${page}`;
-//   fetch(url)
-//     .then(responce => responce.json())
-//     .then(data => {
-//       const images = data.hits;
-//       const totalHits = data.totalHits;
-//       const totalPages = Math.ceil(totalHits / 40);
-//       if (images.length === 0) {
-//         Notiflix.Notify.failure(
-//           'Sorry, there are no images matching your search query. Please try again.'
-//         );
-//         return;
-//       }
-
-//       if (page === totalPages) {
-//         isLastPage = true;
-//         loadMoreBtn.classList.add('is-hidden');
-//       } else {
-//         isLastPage = false;
-//         loadMoreBtn.classList.remove('is-hidden');
-//       }
-//       renderGallery(images);
-//       if (page === 1) {
-//         Notiflix.Notify.info(`We found ${totalHits} images. `);
-//       }
-//     })
-//     .catch(error => {
-//       console.log(error);
-//       Notiflix.Notify.failure(
-//         'Oops! Something went wrong. Please try again later.'
-//       );
-//     });
-// }
-
-// function renderGallery(images) {
-//   images.forEach(image => {
-//     const photoCard = document.createElement('div');
-//     photoCard.classList.add('photo-card');
-//     photoCard.innerHTML = `
-//         <img src='${image.webformatURL}' alt='${image.tags}' loading='lazy' /> <div class='info'><p class='info-item'><b>Likes:</b> ${image.likes}</p><p class='info-item'><b>Views:</b> ${image.views}</p></div>`;
-//     gallery.appendChild(photoCard);
-//   });
-// }
-
-// function clearGallery() {
-//   gallery.innerHTML = '';
-// }
 
 const fetchImages = async () => {
-  const API_KEY = '';
+  const API_KEY = '35196803-673541e2c14d14661bda49ca7';
   const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
 
   try {
-    const responce = await axios.get(URL);
-    const data = responce.data;
+    const response = await axios.get(URL);
+    const data = response.data;
     return data.hits;
   } catch (error) {
     throw new Error(error);
@@ -103,7 +35,7 @@ const createImageCard = ({
 }) => {
   return `
   <div class="photo-card">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" data-source="${largeImageURL}"/>
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" data-source="${largeImageURL}" />
     <div class="info">
       <p class="info-item">
         <b>Likes:</b> ${likes}
@@ -141,6 +73,25 @@ const searchImages = async () => {
     renderImageCards(images);
     page += 1;
     loadMoreBtn.getElementsByClassName.display = 'block';
+
+    const galleryImages = document.querySelectorAll('.gallery-image');
+
+    galleryImages.forEach(image => {
+      image.addEventListener('click', () => {
+        const largeImageURL = image.dataset.source;
+        Notiflix.Modal.image(largeImageURL);
+      });
+
+      image.addEventListener('mouseover', () => {
+        const info = image.nextElementSibling;
+        info.style.display = 'block';
+      });
+
+      image.addEventListener('mouseout', () => {
+        const info = image.nextElementSibling;
+        info.style.display = 'none';
+      });
+    });
   } catch (error) {
     Notiflix.Notify.failure(
       'Oops, something went wrong. Please try again later.'
